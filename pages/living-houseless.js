db86c87be5073187b9e8539085a62d44c408c1f6
Tosin Kuye <tkuye77@gmail.com>
2021-11-30 15:19:33 -0700

    living houseless ep3

--- pages/living-houseless.js
+++ pages/living-houseless.js
@@ -11,10 +11,10 @@ const LivingHouseless = () => {
         <div>
             <h3 className={styles.backButton} onClick={() => Router.push('/')}>Go Back</h3>
             <h1 className={styles.header}>Living Houseless</h1>
-            <p className={styles.description}>Living Houseless is a series that aims to spread awareness about the stories, lives and struggles faced by the vulnerable community. Please contact me at (780) 914-5692 if you have any questions.</p>
+            <p className={styles.description}>Living Houseless is a series that aims to spread awareness about the stories, lives and struggles faced by the vulnerable community. If you're on mobile, swipe left or right to see the next image. If you're on a computer, click the left and right sides of the image to view the next one. Please contact me at (780) 914-5692 if you have any questions.</p>
             <LHCarousel name={"Episode 1"} img_srcs={['/lh1/1.png', '/lh1/2.png','/lh1/3.png','/lh1/4.png','/lh1/5.png','/lh1/6.png', '/lh1/7.png', '/lh1/8.png']}/>
             <LHCarousel name={"Episode 2"} img_srcs={['/lh2/1.png', '/lh2/2.png','/lh2/3.png','/lh2/4.png','/lh2/5.png','/lh2/6.png', '/lh2/7.png', '/lh2/8.png']}/>
-        
+            <LHCarousel name={"Episode 3"} img_srcs={['/lh3/1.png', '/lh3/2.png','/lh3/3.png','/lh3/4.png','/lh3/5.png','/lh3/6.png', '/lh3/7.png', '/lh3/8.png']}/>
         
         </div>
     )
