131c9c0d418f0495b4860c28e632a3f63f38d37a
Tosin Kuye <tkuye77@gmail.com>
2021-12-06 15:56:00 -0700

    Living Houseless Episode 5

--- pages/living-houseless.js
+++ pages/living-houseless.js
@@ -16,6 +16,8 @@ const LivingHouseless = () => {
             <LHCarousel name={"Episode 2"} img_srcs={['/lh2/1.png', '/lh2/2.png','/lh2/3.png','/lh2/4.png','/lh2/5.png','/lh2/6.png', '/lh2/7.png', '/lh2/8.png']}/>
             <LHCarousel name={"Episode 3"} img_srcs={['/lh3/1.png', '/lh3/2.png','/lh3/3.png','/lh3/4.png','/lh3/5.png','/lh3/6.png', '/lh3/7.png', '/lh3/8.png']}/>
             <LHCarousel name={"Episode 4"} img_srcs={['/lh4/1.png', '/lh4/2.png','/lh4/3.png','/lh4/4.png','/lh4/5.png','/lh4/6.png', '/lh4/7.png', '/lh4/8.png', '/lh4/9.png']}/>
+            <LHCarousel name={"Episode 5"} img_srcs={['/lh5/1.png', '/lh5/2.png','/lh5/3.png','/lh5/4.png','/lh5/5.png','/lh5/6.png', '/lh5/7.png', '/lh5/8.png',]}/>
+        
         </div>
     )
 }
